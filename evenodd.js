cf3d5ad2d5db5c121a9126a36a4f051b15159c34
ShivangibaJadeja30 <jadejashivangiba2@gmail.com>
2025-07-15 11:15:03 +0530

    update

---
+++ evenodd.js
@@ -0,0 +1,10 @@
+function evenodd(a){
+    if(a%2==0){
+        console.log("Even..");
+    }
+    else{
+        console.log("Odd..");
+    }
+}
+
+export default evenodd;
--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-let a=7;
-if(a%2==0){
-    console.log("Even..");
-}
-else{
-    console.log("Odd..");
-}
+import evenodd from "./evenodd.js"
+console.log(evenodd(7));
