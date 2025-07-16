5daaa94f35ec54f8495f8fc2f10170e0c8740f29
ShivangibaJadeja30 <jadejashivangiba2@gmail.com>
2025-07-16 13:30:21 +0530

    updated

--- evenodd.js
+++ evenodd.js
@@ -1,10 +1,94 @@
-function evenodd(a){
-    if(a%2==0){
-        console.log("Even..");
+// function evenodd(a){
+//     if(a%2==0){
+//         console.log("Even..");
+//     }
+//     else{
+//         console.log("Odd..");
+//     }
+// }
+
+// export default evenodd;
+
+// export default function Max(b,c){
+
+//     if(b>c)
+//     {
+//         return b+" is max.";
+//     }
+//     else{
+//         return c+" is max.";
+//     }
+// }
+
+
+
+// export default function lp(a){
+//     if(a%4==0){
+//         return a+" is a leap year..";
+//     }
+//     else{
+//          return a+" not a leap year..";
+//     }
+// }
+
+
+// export default function fourth(a){
+//     let sum=0;
+//     for(let i=0;i<=a;i++)
+//     {
+        
+//         sum+=i;
+//     }    
+//     return sum+" is the sum";
+// }
+
+
+
+// export default function five(a)
+// {
+//         let fact=1;
+//         for(let i=1;i<=a;i++)
+//         {
+//             fact*=i;
+//         }
+//         return fact;
+// }
+
+
+// export default function six(a)
+// {
+//     console.log("\n");
+//     for(let i=1;i<=10;i++)
+//     {
+//         console.log(`${a} X ${i} = ${a*i}`);
+//     }
+// }
+
+
+export default function seven(a)
+{
+    while(a>=0)
+    {
+        r=a%10;
+        re=r*10+a;
+        a/=10;
     }
-    else{
-        console.log("Odd..");
-    }
+
 }
 
-export default evenodd;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-import evenodd from "./evenodd.js"
-console.log(evenodd(7));
+// import evenodd from "./evenodd.js"
+// import fourth from "./evenodd.js"
+// console.log(fourth(10));
+
+
+// import five from "./evenodd.js"
+// console.log(five(5));
+
+
+// import six from "./evenodd.js"
+// console.log(six(6));
+
+
+import seven from "./evenodd.js"
+console.log(seven(15236));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
