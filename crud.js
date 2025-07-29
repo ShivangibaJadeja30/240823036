edbf2834304cb3390ead1139aca5bc10fc3fa5c4
ShivnagibaJadeja30 <jadejashivnagiba2.com>
2025-07-29 10:52:29 +0530

    updated

---
+++ crud.js
@@ -0,0 +1,12 @@
+// const express=require("express");
+// const app = express();
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello Everyone !");
+// });
+// app.get("/home",(res,res)=>{
+//     res.send("This is my Home page.");
+// });
+// app.listen(80,()=>{
+//     console.log("Server running...at http://localhost:80");
+// });
