6c9987f885f87507c4ce292eae7fbdc5be6c54ba
ShivangibaJadeja30 <jadejashivangiba2@gmail.com>
2025-08-02 09:20:15 +0530

    update

--- crud.js
+++ crud.js
@@ -1,28 +1,12 @@
-const express=require("express");
-const app = express();
+const express = require("express");
+const {}=require("")
 
-app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("All students");
-});
+// app.get("/",(req,res)=>{
+//     res.send("Hello Everyone Welcome to Mca !");
+// });
+app.get("/");
 
-app.get("/show/:id",(req,res)=>{
-    res.send(`Show student ID : ${req.params.id}`);
-});
-
-app.post("/store",(req,res)=>{
-    res.send("create students");
-});
-
-app.put("/update/:id",(req,res)=>{
-    res.send("update students");
-});
-
-app.delete("/delete/:id",(req,res)=>{
-    res.send("delete students");
-});
-
-app.listen(3602,()=>{
-    console.log("Server running...at http://localhost:3602");
+app.listen(2002,()=>{
+    console.log("Server running at....... http://localhost:2002");
 });
