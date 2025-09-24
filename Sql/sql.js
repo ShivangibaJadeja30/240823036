b28e7acea1352e05d39403cdada2a9e4d5ded483
ShivangibaJadeja30 <jadejashivangiba2@gmail.com>
2025-09-24 13:06:45 +0530

    sql

--- Sql/sql.js
+++ Sql/sql.js
@@ -1,14 +1,35 @@
-const express=require("express");
-const app=
+// const {Sequalize}=require('sequelize');
+// const sequelize = new Sequelize("node","root","",
+//     {   host:'localhost',
+//         dialect:'mysql'
+    
+//     })
+// sequelize.authenticate()
+//     .then(()=>{
+//     console.log("connected");
+//     })
+//     .catch((err)=>{
+//     console.error("Error",err);
+// });
+// module.exports=sequelize;
 
-const seq=require("sequalize");
-const db=new seq("sql","root","",{host:"localhost",dialect:"mysql"});
-db.authenticate()
-    .then(()=>{
-        console.log("Success....");
-    })
-    .catch((err)=>{
-        console.error("Error...",error);
-    });
 
+const sequelize=require('sequelize');
+const db=new sequelize("node");
 
+// const {Sequelize}= require('sequelize');
+
+// const sequelize = new Sequelize('node',"root","",{
+//     host:'localhost',
+//     dialect:'mysql'
+// })
+
+// sequelize.authenticate()
+//     .then(()=>{
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(err=>{
+//         console.error('Unable to connect to the database:', err);
+//     });
+
+// module.exports= sequelize;
